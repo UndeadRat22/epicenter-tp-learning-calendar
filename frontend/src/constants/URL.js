70cdd9e8b4d9c1f78709924b6e01220474dd2524
Tomas Zaicevas <tozaicevas@gmail.com>
2020-04-25 15:40:27 +0300

    feature-toggles port fix

--- frontend/src/constants/URL.js
+++ frontend/src/constants/URL.js
@@ -1,4 +1,4 @@
-const LOCAL_FEATURES_URL = 'http://sasyska.lt/api/client/features';
+const LOCAL_FEATURES_URL = 'http://sasyska.lt:4242/api/client/features';
 const BACKEND_API_URL = 'https://c5cddfe5.ngrok.io';
 
 export { LOCAL_FEATURES_URL, BACKEND_API_URL };
