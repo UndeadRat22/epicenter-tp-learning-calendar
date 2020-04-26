44753fd5b3bda5badbf31b0f0d71e35d28f83801
Tomas Zaicevas <tozaicevas@gmail.com>
2020-04-26 21:08:52 +0300

    page refresh will route logged user to the same route

--- frontend/src/router/ProtectedRoute.js
+++ frontend/src/router/ProtectedRoute.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { LOGGED_IN } from '../constants/AuthStatus';
+import { LOGGED_IN, LOADING_FETCH_SELF } from '../constants/AuthStatus';
+import LoadingIndicator from '../components/LoadingIndicator';
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
   const auth = useSelector(state => state.auth);
   const isAuthenticated = auth.status === LOGGED_IN;
+
 
   return (
     <Route
@@ -13,6 +15,8 @@ const ProtectedRoute = ({ component: Component, ...rest }) => {
       render={props => {
         if (isAuthenticated)
           return <Component {...props} />;
+        if (auth.status === LOADING_FETCH_SELF)
+          return <LoadingIndicator text="Loading session..." />;
 
         return (
           <Redirect to={
