7f5d170b7dd0889091dfc446dcf9dc4a0b39c7ba
Tomas Zaicevas <tozaicevas@gmail.com>
2020-05-23 20:12:06 +0300

    date format changed in learning day

--- frontend/src/components/homePage/LearningDay.js
+++ frontend/src/components/homePage/LearningDay.js
@@ -21,7 +21,7 @@ LearningDay.navigate = (date, action) => {
 };
 
 LearningDay.title = date => {
-  return date.toLocaleDateString();
+  return date.toISOString().slice(0, 10);
 };
 
 export default LearningDay;
