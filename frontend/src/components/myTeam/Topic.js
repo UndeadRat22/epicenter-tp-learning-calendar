671862c2875de539b40ef5e31d3827a9fe2f7790
Tomas Zaicevas <tozaicevas@gmail.com>
2020-05-25 00:17:26 +0300

    <Tooltip/> with topic description in MyTeam + only subject required in CreateTopic

--- frontend/src/components/myTeam/Topic.js
+++ frontend/src/components/myTeam/Topic.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDrag } from 'react-dnd';
-import { Text } from 'wix-style-react';
+import { Text, Tooltip } from 'wix-style-react';
 import s from './styles.scss';
 import { TOPIC } from '../../constants/DraggableTypes';
 
@@ -14,9 +14,19 @@ const Topic = ({ topic }) => {
 
   return (
     <div className={isDragging ? s.draggedTopic : s.topic} ref={drag}>
-      <Text>
-        {topic.subject}
-      </Text>
+      {isDragging
+        ? (
+          <Text>
+            {topic.subject}
+          </Text>
+        )
+        : (
+          <Tooltip content="very very looong long description, we need to get this from backend :)" placement="left-end">
+            <Text>
+              {topic.subject}
+            </Text>
+          </Tooltip>
+        )}
     </div>
   );
 };
