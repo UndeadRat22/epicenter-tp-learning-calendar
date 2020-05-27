b0639aa849463e73a40434d87759908c0cc99e2f
Valdas Rakutis <valdas@rakutis.lt>
2020-05-27 18:42:41 +0300

    Add new strategy for filtering employee status collection

---
+++ backend/LearningCalendar/Epicenter.Service.Strategy.Interface/Topic/IEmployeeCollectionTopicProgressStatusStrategy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Epicenter.Domain.Entity.LearningCalendar;
+
+namespace Epicenter.Service.Strategy.Interface.Topic
+{
+    public interface IEmployeeCollectionTopicProgressStatusStrategy
+    {
+        EmployeeCollectionStatus GetEmployeeCollectionStatusForTopic(
+            IEnumerable<Employee> employees, 
+            Domain.Entity.LearningCalendar.Topic topic);
+    }
+
+    public class EmployeeCollectionStatus
+    {
+        public List<Employee> LearnedEmployees { get; set; } = new List<Employee>();
+        public List<Employee> PlannedEmployees { get; set; } = new List<Employee>();
+        public List<Employee> OtherEmployees { get; set; } = new List<Employee>();
+    }
+}
---
+++ backend/LearningCalendar/Epicenter.Service.Strategy/Topic/EmployeeCollectionTopicProgressStatusStrategy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Epicenter.Domain.Entity.LearningCalendar;
+using Epicenter.Service.Strategy.Interface.Topic;
+
+namespace Epicenter.Service.Strategy.Topic
+{
+    public class EmployeeCollectionTopicProgressStatusStrategy : IEmployeeCollectionTopicProgressStatusStrategy
+    {
+        private readonly IEmployeeTopicProgressStatusStrategy _employeeTopicProgressStatusStrategy;
+
+        public EmployeeCollectionTopicProgressStatusStrategy(IEmployeeTopicProgressStatusStrategy employeeTopicProgressStatusStrategy)
+        {
+            _employeeTopicProgressStatusStrategy = employeeTopicProgressStatusStrategy;
+        }
+
+        public EmployeeCollectionStatus GetEmployeeCollectionStatusForTopic(IEnumerable<Employee> employees, Domain.Entity.LearningCalendar.Topic topic)
+        {
+            var collectionStatus = new EmployeeCollectionStatus();
+
+            foreach (Employee employee in employees)
+            {
+                var employeeStatus = _employeeTopicProgressStatusStrategy.GetStatus(employee, topic);
+                switch (employeeStatus)
+                {
+                    case Status.NotPlanned:
+                        collectionStatus.OtherEmployees.Add(employee);
+                        break;
+                    case Status.Planned:
+                        collectionStatus.PlannedEmployees.Add(employee);
+                        break;
+                    case Status.Learned:
+                        collectionStatus.LearnedEmployees.Add(employee);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            return collectionStatus;
+        }
+    }
+}
