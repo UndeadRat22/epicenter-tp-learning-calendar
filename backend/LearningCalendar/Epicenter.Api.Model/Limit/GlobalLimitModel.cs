9fb58ab3c2980b76e14e3b548ba155425e2d41fa
Valdas Rakutis <valdas@rakutis.lt>
2020-05-28 21:55:45 +0300

    Get Global limit, update postman collection

---
+++ backend/LearningCalendar/Epicenter.Api.Model/Limit/GlobalLimitModel.cs
@@ -0,0 +1,16 @@
+﻿using Epicenter.Service.Interface.Operations.Limit;
+
+namespace Epicenter.Api.Model.Limit
+{
+    public class GlobalLimitModel
+    {
+        public GlobalLimitModel(GetGlobalLimitOperationResponse response)
+        {
+            DaysPerQuarter = response.DaysPerQuarter;
+            TopicsPerDay = response.TopicsPerDay;
+        }
+
+        public int DaysPerQuarter { get; set; }
+        public int TopicsPerDay { get; set; }
+    }
+}
---
+++ backend/LearningCalendar/Epicenter.Service/Operations/Limit/GetGlobalLimitOperation.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Epicenter.Persistence.Interface.Repository.LearningCalendar;
+using Epicenter.Service.Interface.Operations.Limit;
+
+namespace Epicenter.Service.Operations.Limit
+{
+    public class GetGlobalLimitOperation : Operation, IGetGlobalLimitOperation
+    {
+        private readonly ILimitRepository _limitRepository;
+
+        public GetGlobalLimitOperation(ILimitRepository limitRepository)
+        {
+            _limitRepository = limitRepository;
+        }
+        public async Task<GetGlobalLimitOperationResponse> Execute()
+        {
+            var globalLimit = await _limitRepository.GetGlobalAsync();
+            return new GetGlobalLimitOperationResponse
+            {
+                DaysPerQuarter = globalLimit.DaysPerQuarter,
+                TopicsPerDay = globalLimit.TopicsPerDay
+            };
+        }
+    }
+}
