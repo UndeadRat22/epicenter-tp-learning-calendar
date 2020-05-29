b8acf0358ef453a1de94a50635143be1f408c190
Tomas Zaicevas <tozaicevas@gmail.com>
2020-05-29 08:42:41 +0300

    emulate-slow-internet feature toggle implemented

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,7 @@
 import Axios from 'axios';
 import { BACKEND_API_URL } from './constants/URL';
 import cookies from './utils/cookies';
+import FeatureToggles from './utils/FeatureToggles';
 
 const BASE_URL = `${BACKEND_API_URL}/api/`;
 
@@ -18,12 +19,15 @@ const setupAxios = () => {
     error => Promise.reject(error),
   );
 
-  Axios.interceptors.response.use(response => {
+  Axios.interceptors.response.use(async response => {
     const { config: { baseURL }, config: { url } } = response;
 
     if (baseURL !== BASE_URL || url === 'auth/refresh' || url === 'employees/self' || url === 'auth/login'
       || url === 'auth/password' || url === 'auth/register')
       return response;
+
+    if (FeatureToggles.isOn('emulate-slow-internet'))
+      await new Promise((resolve, reject) => setTimeout(() => resolve(), 3000));
 
     Axios.get('auth/refresh').then(refreshResponse => {
       const { token, expires } = refreshResponse.data;
