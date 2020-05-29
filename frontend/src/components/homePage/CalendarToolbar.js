769019d6f783f38b936afe7f5fc62e606cfd4f49
Tomas Zaicevas <tozaicevas@gmail.com>
2020-05-29 11:55:52 +0300

    black arrow buttons in calendar

--- frontend/src/components/homePage/CalendarToolbar.js
+++ frontend/src/components/homePage/CalendarToolbar.js
@@ -1,5 +1,12 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import './Calendar.global.scss';
+import {
+  IconButton, Col, Container, Row, Text,
+} from 'wix-style-react';
+import ArrowLeft from 'wix-ui-icons-common/ArrowLeft';
+import ArrowRight from 'wix-ui-icons-common/ArrowRight';
+import './CalendarToolbar.global.scss';
 
 const navigate = {
   PREVIOUS: 'PREV',
@@ -8,14 +15,17 @@ const navigate = {
 
 const CalendarToolbar = ({ onNavigate, label }) => {
   return (
-    <div className="rbc-toolbar">
-      <span className="rbc-btn-group">
-        <button type="button" onClick={() => onNavigate(navigate.PREVIOUS)}>Prev</button>
-      </span>
-      <span className="rbc-toolbar-label">{label}</span>
-      <span className="rbc-btn-group">
-        <button type="button" onClick={() => onNavigate(navigate.NEXT)}>Next</button>
-      </span>
+    <div id="textbox">
+      <div className="alignleft" role="button" onClick={() => onNavigate(navigate.PREVIOUS)} tabIndex={0}>
+        <ArrowLeft size={32} />
+      </div>
+      <Text weight="bold" className="aligncenter">{label}</Text>
+      <div className="alignright" role="button" onClick={() => onNavigate(navigate.NEXT)} tabIndex={0}>
+        <ArrowRight size={32} />
+      </div>
+      {/* <IconButton priority="secondary" style={{ marginBottom: 4 }} className="alignleft" as="button" onClick={() => alert('ha')}><ArrowLeft /></IconButton>
+      <Text weight="bold" className="aligncenter">{label}</Text>
+      <IconButton priority="secondary" className="alignright" style={{ marginBottom: 4 }} as="button" onClick={() => alert('ha')}><ArrowRight /></IconButton> */}
     </div>
   );
 };
