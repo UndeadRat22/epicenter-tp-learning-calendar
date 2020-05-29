536726efe308d7520ea42cce9275f6cc6ed4a959
Aurelija Cygaite <Mac@MacBooks-MacBook-Pro-2.local>
2020-05-29 14:03:06 +0300

    cookies expired

--- frontend/src/components/CookiesContainer.js
+++ frontend/src/components/CookiesContainer.js
@@ -2,6 +2,7 @@
 import React, { useCallback, useState } from 'react';
 import { Notification } from 'wix-style-react';
 import cookies from '../utils/cookies';
+import s from './Cookies.global.scss';
 
 const CookiesContainer = () => {
   const isAccepted = cookies.get('acceptCookiesPolicy');
@@ -9,8 +10,11 @@ const CookiesContainer = () => {
   const [, updateState] = useState();
   const forceUpdate = useCallback(() => updateState({}), []);
 
+  const today = new Date();
+  const nextWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7);
+
   const onAccept = () => {
-    cookies.set('acceptCookiesPolicy', 'accepted', { path: '/', sameSite: true });
+    cookies.set('acceptCookiesPolicy', 'accepted', { path: '/', expires: nextWeek, sameSite: true });
     forceUpdate();
   };
 
@@ -18,18 +22,20 @@ const CookiesContainer = () => {
     return null;
 
   return (
-    <Notification
-      type="local"
-      theme="warning"
-      show
-      actionText="Accept"
-      appearance="premium"
-    >
-      <Notification.TextLabel>This website use cookies!</Notification.TextLabel>
-      <Notification.ActionButton onClick={() => onAccept()}>
-        Accept
-      </Notification.ActionButton>
-    </Notification>
+    <div className={s.snackbar}>
+      <Notification
+        type="local"
+        theme="warning"
+        show
+        actionText="Accept"
+        appearance="premium"
+      >
+        <Notification.TextLabel>This website use cookies!</Notification.TextLabel>
+        <Notification.ActionButton onClick={() => onAccept()}>
+          Accept
+        </Notification.ActionButton>
+      </Notification>
+    </div>
   );
 };
 
